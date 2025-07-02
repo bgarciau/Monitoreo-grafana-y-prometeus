c8a2c8919fa690b017cfdacebb5f3229d45b149f
Brayan Garcia <107225344+bgarciau@users.noreply.github.com>
2025-07-01 19:34:31 -0500

    try

---
+++ app/app.py
@@ -0,0 +1,20 @@
+# app/app.py
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+import prometheus_client
+
+app = Flask(__name__)
+REQUEST_COUNT = Counter("request_count", "App Request Count")
+
+@app.route("/")
+def hello():
+    REQUEST_COUNT.inc()
+    return "Hello from the app!"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": "text/plain"}
+
+if __name__ == "__main__":
+    prometheus_client.start_http_server(8000)
+    app.run(host="0.0.0.0", port=5000)
